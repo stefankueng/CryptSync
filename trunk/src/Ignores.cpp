7d6009daef8be7d75bdec9fac88f7783c4ed822b
steveking <steveking@c762c618-af21-49ec-a0d7-fe1e9d614c35>
2012-11-04 16:57:44 +0000

    Move stringtok() to the StringUtils.h file

--- trunk/src/Ignores.cpp
+++ trunk/src/Ignores.cpp
@@ -21,38 +21,6 @@
 #include "Registry.h"
 #include "StringUtils.h"
 
-template <typename Container>
-void stringtok(Container &container, const std::wstring  &in, bool trim,
-    const wchar_t * const delimiters = L"|")
-{
-    const std::string::size_type len = in.length();
-    std::string::size_type i = 0;
-
-    while ( i < len )
-    {
-        if (trim)
-        {
-            // eat leading whitespace
-            i = in.find_first_not_of (delimiters, i);
-            if (i == std::string::npos)
-                return;   // nothing left but white space
-        }
-
-        // find the end of the token
-        std::string::size_type j = in.find_first_of (delimiters, i);
-
-        // push token
-        if (j == std::string::npos) {
-            container.push_back (in.substr(i));
-            return;
-        } else
-            container.push_back (in.substr(i, j-i));
-
-        // set up for next loop
-        i = j + 1;
-    }
-}
-
 
 CIgnores::CIgnores(void)
 {
