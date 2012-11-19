48000c86e128d6bbe2ab8a1b9a8cca6fbf63cb8e
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2012-11-19 19:04:27 +0000

    put the password in quotes before passing it to the command line.

--- src/FolderSync.cpp
+++ src/FolderSync.cpp
@@ -473,7 +473,7 @@ bool CFolderSync::EncryptFile( const std::wstring& orig, const std::wstring& cry
     // 7zip 9.30 has an option "-stl" which sets the timestamp of the archive
     // to the most recent one of the compressed files
     // add this flag as soon as 9.30 is stable and officially released.
-    swprintf_s(cmdlinebuf.get(), buflen, L"\"%s\" a -t7z \"%s\" \"%s\" -mx9 -p%s -mhe=on -w", m_sevenzip.c_str(), cryptname.c_str(), orig.c_str(), password.c_str());
+    swprintf_s(cmdlinebuf.get(), buflen, L"\"%s\" a -t7z \"%s\" \"%s\" -mx9 -p\"%s\" -mhe=on -w", m_sevenzip.c_str(), cryptname.c_str(), orig.c_str(), password.c_str());
     bool bRet = Run7Zip(cmdlinebuf.get(), targetfolder);
     if (!bRet)
     {
@@ -516,7 +516,7 @@ bool CFolderSync::DecryptFile( const std::wstring& orig, const std::wstring& cry
     std::wstring targetfolder = orig.substr(0, slashpos);
     int buflen = orig.size() + crypt.size() + password.size() + 1000;
     std::unique_ptr<wchar_t[]> cmdlinebuf(new wchar_t[buflen]);
-    swprintf_s(cmdlinebuf.get(), buflen, L"\"%s\" e \"%s\" -o\"%s\" -p%s -y", m_sevenzip.c_str(), crypt.c_str(), targetfolder.c_str(), password.c_str());
+    swprintf_s(cmdlinebuf.get(), buflen, L"\"%s\" e \"%s\" -o\"%s\" -p\"%s\" -y", m_sevenzip.c_str(), crypt.c_str(), targetfolder.c_str(), password.c_str());
     bool bRet = Run7Zip(cmdlinebuf.get(), targetfolder);
     if (!bRet)
     {
