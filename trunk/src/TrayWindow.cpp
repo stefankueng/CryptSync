d6de4f685ccd6d9ff561a254260276fe6729f30c
steveking <steveking@c762c618-af21-49ec-a0d7-fe1e9d614c35>
2012-12-07 21:59:32 +0000

    When starting with Windows, do not start a full sync immediately: give the could drives time to sync first.

--- trunk/src/TrayWindow.cpp
+++ trunk/src/TrayWindow.cpp
@@ -172,7 +172,6 @@ LRESULT CALLBACK CTrayWindow::WinMsgHandler(HWND hwnd, UINT uMsg, WPARAM wParam,
             SetTimer(*this, TIMER_DETECTCHANGES, TIMER_DETECTCHANGESINTERVAL, NULL);
             SetTimer(*this, TIMER_FULLSCAN, TIMER_FULLSCANINTERVAL, NULL);
             unsigned int threadId = 0;
-            _beginthreadex(NULL, 0, UpdateCheckThreadEntry, this, 0, &threadId);
             if (!m_bTrayMode)
                 ::PostMessage(*this, WM_COMMAND, MAKEWPARAM(IDM_OPTIONS, 1), 0);
         }
