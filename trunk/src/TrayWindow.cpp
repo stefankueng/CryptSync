3e67cb887058251a11fabbb06e9201b2f1eeea0d
XhmikosR <XhmikosR@c762c618-af21-49ec-a0d7-fe1e9d614c35>
2012-12-26 10:37:52 +0000

    replace ZeroMemory with SecureZeroMemory

--- trunk/src/TrayWindow.cpp
+++ trunk/src/TrayWindow.cpp
@@ -54,7 +54,7 @@ DWORD CTrayWindow::GetDllVersion(LPCTSTR lpszDllName)
             DLLVERSIONINFO dvi;
             HRESULT hr;
 
-            ZeroMemory(&dvi, sizeof(dvi));
+            SecureZeroMemory(&dvi, sizeof(dvi));
             dvi.cbSize = sizeof(dvi);
 
             hr = (*pDllGetVersion)(&dvi);
@@ -116,7 +116,7 @@ void CTrayWindow::ShowTrayIcon()
 {
     // since our main window is hidden most of the time
     // we have to add an auxiliary window to the system tray
-    ZeroMemory(&niData,sizeof(NOTIFYICONDATA));
+    SecureZeroMemory(&niData,sizeof(NOTIFYICONDATA));
 
     ULONGLONG ullVersion = GetDllVersion(_T("Shell32.dll"));
     if (ullVersion >= MAKEDLLVERULL(6,0,0,0))
