9304e684a780fc74937f82ef601d3eff07b1657d
steveking <steveking@c762c618-af21-49ec-a0d7-fe1e9d614c35>
2013-06-16 12:01:30 +0000

    tag the 1.0.8 release

---
+++ tags/1.0.8/src/CryptSync.cpp
@@ -0,0 +1,159 @@
+// CryptSync - A folder sync tool with encryption
+
+// Copyright (C) 2012-2013 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "stdafx.h"
+#include "CmdLineParser.h"
+#include "TrayWindow.h"
+#include "Ignores.h"
+#include "resource.h"
+
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+TCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+CPairs g_pairs;
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+std::wstring GetMutexID()
+{
+    std::wstring t;
+    CAutoGeneralHandle token;
+    BOOL result = OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, token.GetPointer());
+    if(result)
+    {
+        DWORD len = 0;
+        GetTokenInformation(token, TokenStatistics, NULL, 0, &len);
+        if (len >= sizeof (TOKEN_STATISTICS))
+        {
+            std::unique_ptr<BYTE[]> data (new BYTE[len]);
+            GetTokenInformation(token, TokenStatistics, data.get(), len, &len);
+            LUID uid = ((PTOKEN_STATISTICS)data.get())->AuthenticationId;
+            wchar_t buf[100] = {0};
+            swprintf_s(buf, L"{81C34844-03AC-4DAA-865B-BC51F07F7F9E}-%08x%08x", uid.HighPart, uid.LowPart);
+            t = buf;
+        }
+    }
+    return t;
+}
+
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                       HINSTANCE hPrevInstance,
+                       LPTSTR    lpCmdLine,
+                       int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(nCmdShow);
+
+    SetDllDirectory(L"");
+    OleInitialize(NULL);
+    CoInitializeEx(0, COINIT_APARTMENTTHREADED);
+    LoadLibrary(L"riched32.dll");
+
+
+    CCmdLineParser parser(lpCmdLine);
+    if (parser.HasKey(L"?") || parser.HasKey(L"help"))
+    {
+        std::wstring sInfo = L"/src      : path to source folder with original content\n"
+                             L"/dst      : path to encrpyted folder\n"
+                             L"/pw       : password for encryption\n"
+                             L"/cpy      : copy only\n"
+                             L"/encnames : encrypt file and folder names\n"
+                             L"/mirror   : mirror only from src to dst\n"
+                             L"/use7z    : use .7z instead of .cryptsync extension\n"
+                             L"/ignore   : ignore patterns\n"
+                             L"\n"
+                             L"/syncall  : syncs all set up pairs and then exists\n"
+                             L"/progress : shows a progress dialog while syncing";
+        MessageBox(NULL, sInfo.c_str(), L"CryptSync Command Line Options", MB_ICONINFORMATION);
+        return 1;
+    }
+    if (parser.HasVal(L"src") && parser.HasVal(L"dst"))
+    {
+        std::wstring src =   parser.GetVal(L"src");
+        std::wstring dst =   parser.GetVal(L"dst");
+        std::wstring pw  =   parser.HasVal(L"pw") ? parser.GetVal(L"pw") : L"";
+        std::wstring cpy =   parser.HasVal(L"cpy") ? parser.GetVal(L"cpy") : L"";
+        bool encnames    = !!parser.HasKey(L"encnames");
+        bool mirror      = !!parser.HasKey(L"mirror");
+        bool use7z       = !!parser.HasKey(L"use7z");
+        std::wstring ign =   parser.HasVal(L"ignore") ? parser.GetVal(L"ignore") : L"";
+
+        CIgnores::Instance().Reload();
+        if (!ign.empty())
+            CIgnores::Instance().Reload(ign);
+
+        CPairs pair;
+        pair.clear();
+        pair.AddPair(src, dst, pw, cpy, encnames, mirror, use7z);
+        CFolderSync foldersync;
+        foldersync.SyncFoldersWait(pair, parser.HasKey(L"progress") ? GetDesktopWindow() : NULL);
+        return 1;
+    }
+    if (parser.HasKey(L"syncall"))
+    {
+        CIgnores::Instance().Reload();
+
+        CPairs pair;
+        CFolderSync foldersync;
+        foldersync.SyncFoldersWait(pair, parser.HasKey(L"progress") ? GetDesktopWindow() : NULL);
+        return 1;
+    }
+
+    MSG msg;
+
+    HANDLE hReloadProtection = ::CreateMutex(NULL, FALSE, GetMutexID().c_str());
+    if ((!hReloadProtection) || (GetLastError() == ERROR_ALREADY_EXISTS))
+    {
+        // An instance of CryptSync is already running
+        CoUninitialize();
+        OleUninitialize();
+        return 0;
+    }
+
+
+    CTrayWindow trayWindow(hInstance);
+    trayWindow.ShowDialogImmediately(!parser.HasKey(L"tray"));
+
+    if (trayWindow.RegisterAndCreateWindow())
+    {
+        HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CryptSync));
+        // Main message loop:
+        while (GetMessage(&msg, NULL, 0, 0))
+        {
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        return (int) msg.wParam;
+    }
+    CoUninitialize();
+    OleUninitialize();
+    return 1;
+}
