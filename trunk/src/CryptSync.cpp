a7588e9b91d9dd7d0985ff4d137837860485e25f
steveking <steveking@c762c618-af21-49ec-a0d7-fe1e9d614c35>
2013-09-07 13:43:28 +0000

    * create a registry entry under HKLM when installing CryptSync * check for the HKLM registry key to determine where to write the logfile to (Fixes issue #58) : Place logfile in a writable location for limited (non-administrator) users

--- trunk/src/CryptSync.cpp
+++ trunk/src/CryptSync.cpp
@@ -101,7 +101,45 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     int maxlog       =   parser.HasVal(L"maxlog") ? parser.GetLongVal(L"maxlog") : 10000;
 
     if (lp.empty())
-        lp = CPathUtils::GetAppDataPath() + L"\\CryptSync.log";
+    {
+        // in case CryptSync was installed with the installer, there's a registry
+        // entry made by the installer.
+        HKEY subKey = nullptr;
+        LONG result = RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"Software\\CryptSync", 0, KEY_READ, &subKey);
+        if (result != ERROR_SUCCESS)
+        {
+            // CryptSync was not installed, which means it's run as a portable app:
+            // use the same directory as the exe is in to store the application data
+            DWORD len = 0;
+            DWORD bufferlen = MAX_PATH;     // MAX_PATH is not the limit here!
+            std::unique_ptr<wchar_t[]> path(new wchar_t[bufferlen]);
+            do
+            {
+                bufferlen += MAX_PATH;      // MAX_PATH is not the limit here!
+                path = std::unique_ptr<wchar_t[]>(new wchar_t[bufferlen]);
+                len = GetModuleFileName(hInst, path.get(), bufferlen);
+            } while(len == bufferlen);
+            std::wstring sPath = path.get();
+            sPath = sPath.substr(0, sPath.find_last_of('\\'));
+            lp = CPathUtils::GetLongPathname(sPath);
+            lp += L"\\CryptSync.log";
+        }
+        else
+        {
+            RegCloseKey(subKey);
+            // CryptSync is installed: we must not store the application data
+            // in the same directory as the exe is but in %APPDATA%\CryptSync instead
+            PWSTR outpath = nullptr;
+            if (SUCCEEDED(SHGetKnownFolderPath(FOLDERID_RoamingAppData, KF_FLAG_CREATE, NULL, &outpath)))
+            {
+                lp = outpath;
+                lp += L"\\CryptSync";
+                lp = CPathUtils::GetLongPathname(lp);
+                lp += L"\\CryptSync.log";
+                CoTaskMemFree(outpath);
+            }
+        }
+    }
 
     CCircularLog::Instance().Init(lp, maxlog);
     CCircularLog::Instance()(L"Starting CryptSync");
