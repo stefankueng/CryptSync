4d0b22caa0c2bebac07d7deb14697be2796b4802
steveking <steveking@c762c618-af21-49ec-a0d7-fe1e9d614c35>
2013-10-12 07:52:14 +0000

    Tag the 1.1.3 release

---
+++ tags/1.1.3/src/AboutDlg.cpp
@@ -0,0 +1,69 @@
+// CryptSync - A folder sync tool with encryption
+
+// Copyright (C) 2012-2013 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "stdafx.h"
+#include "resource.h"
+#include "AboutDlg.h"
+#include "version.h"
+#include <string>
+
+
+CAboutDlg::CAboutDlg(HWND hParent)
+    : m_hParent(hParent)
+{
+}
+
+CAboutDlg::~CAboutDlg(void)
+{
+}
+
+LRESULT CAboutDlg::DlgFunc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+        {
+            InitDialog(hwndDlg, IDI_CryptSync);
+            TCHAR buf[MAX_PATH] = {0};
+            _stprintf_s(buf, _countof(buf), _T("CryptSync version %ld.%ld.%ld.%ld"), CS_VERMAJOR, CS_VERMINOR, CS_VERMICRO, CS_VERBUILD);
+            SetDlgItemText(*this, IDC_VERSIONINFO, buf);
+            SetDlgItemText(*this, IDC_DATE, _T(CS_VERDATE));
+            m_link.ConvertStaticToHyperlink(hwndDlg, IDC_WEBLINK, _T("http://stefanstools.sourceforge.net"));
+        }
+        return TRUE;
+    case WM_COMMAND:
+        return DoCommand(LOWORD(wParam), HIWORD(wParam));
+    default:
+        return FALSE;
+    }
+}
+
+LRESULT CAboutDlg::DoCommand(int id, int /*msg*/)
+{
+    switch (id)
+    {
+    case IDOK:
+        // fall through
+    case IDCANCEL:
+        EndDialog(*this, id);
+        break;
+    }
+    return 1;
+}
