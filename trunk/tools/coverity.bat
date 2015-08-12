b25ea02a8adc89f95d8953375e1ee30b586a2324
xhmikosr <xhmikosr@c762c618-af21-49ec-a0d7-fe1e9d614c35>
2015-08-12 15:03:47 +0000

    Update tools/coverity.bat for MSVC 2015.

--- trunk/tools/coverity.bat
+++ trunk/tools/coverity.bat
@@ -26,7 +26,7 @@ if exist "CryptSync.tgz"  del "CryptSync.tgz"
 
 
 :main
-call "%VS120COMNTOOLS%\vsvars32.bat"
+call "%VS140COMNTOOLS%\vsvars32.bat"
 if %ERRORLEVEL% neq 0 (
   echo vsvars32.bat call failed.
   goto End
